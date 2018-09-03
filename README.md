c053acefbd0de0a02cbe7307bdedc38e42f298d1
Richard Davey <rich@photonstorm.com>
2018-09-03 14:52:46 +0100

    Phaser 3 custom build template

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# phaser3-custom-build
+# Phaser 3 Custom Build Template
+
+This project can be used as the basis for a custom build of Phaser 3.
+
+Please see the tutorial in issue 127 of the Phaser World newsletter for full details.
---
+++ phaser-custom.js
@@ -0,0 +1,34 @@
+require('polyfills');
+
+var CONST = require('const');
+var Extend = require('utils/object/Extend');
+
+var Phaser = {
+
+   Cameras: {
+       Scene2D: require('cameras/2d')
+   },
+   Events: require('events/EventEmitter'),
+   Game: require('boot/Game'),
+   GameObjects: {
+       DisplayList: require('gameobjects/DisplayList'),
+       UpdateList: require('gameobjects/UpdateList'),
+       Graphics: require('gameobjects/graphics/Graphics.js'),
+       Factories: {
+           Graphics: require('gameobjects/graphics/GraphicsFactory')
+       },
+       Creators: {
+           Graphics: require('gameobjects/graphics/GraphicsCreator')
+       }
+   }
+};
+
+//   Merge in the consts
+
+Phaser = Extend(false, Phaser, CONST);
+
+//  Export it
+
+module.exports = Phaser;
+
+global.Phaser = Phaser;
