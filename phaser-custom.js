389b72e70accf15d1bdb14c093852ef652f18f17
PatrickSachs <kontakt@patrick-sachs.de>
2019-03-16 23:31:28 +0100

    Made example work

--- phaser-custom.js
+++ phaser-custom.js
@@ -12,16 +12,29 @@ var Phaser = {
     Events: require('phaser/src/events/EventEmitter'),
     Game: require('phaser/src/core/Game'),
     GameObjects: {
+        // Add the these two to make the indexsprite.html test work!
+        Image: require('phaser/src/gameobjects/image/Image'),
+        Sprite: require('phaser/src/gameobjects/sprite/Sprite'),
+
         DisplayList: require('phaser/src/gameobjects/DisplayList'),
         UpdateList: require('phaser/src/gameobjects/UpdateList'),
         Graphics: require('phaser/src/gameobjects/graphics/Graphics.js'),
         Factories: {
+            // Add the these two to make the indexsprite.html test work!
+            Image: require('phaser/src/gameobjects/image/ImageFactory'),
+            Sprite: require('phaser/src/gameobjects/sprite/SpriteFactory'),
+
             Graphics: require('phaser/src/gameobjects/graphics/GraphicsFactory')
         },
         Creators: {
+            // Add the these two to make the indexsprite.html test work!
+            Image: require('phaser/src/gameobjects/image/ImageCreator'),
+            Sprite: require('phaser/src/gameobjects/sprite/SpriteCreator'),
+            
             Graphics: require('phaser/src/gameobjects/graphics/GraphicsCreator')
         }
     },
+    Loader: require('phaser/src/loader'),
     Math: {
         Between: require('phaser/src/math/Between')
     }
